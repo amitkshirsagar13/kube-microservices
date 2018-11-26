ee389c0b80c338d847c343a7d23b18d679def72e
Amit Kshirsagar <amitkshirsagar13@users.noreply.github.com>
2018-11-26 13:10:01 +0530

    Update express-server.js

--- express/express-server.js
+++ express/express-server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const config = require('../config');
 const app = express();
-const port = 3000;
+
+const port = config.app.port;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = app;
