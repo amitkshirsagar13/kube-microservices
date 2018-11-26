4c8bd5a07af4a30e794effe9100f9617aa367b27
Amit Kshirsagar <amitkshirsagar13@users.noreply.github.com>
2018-11-26 17:54:17 +0530

    Update express-server.js

--- nodejs-service/express/express-server.js
+++ nodejs-service/express/express-server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const config = require('../config');
 const app = express();
+app.use(cors());
 
 const port = config.app.port;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
