2731d5ef2d3d60767b9770dde0aea21dd52c7acc
Amit Kshirsagar <amitkshirsagar13@users.noreply.github.com>
2018-11-26 17:59:24 +0530

    Update express-server.js

--- nodejs-service/express/express-server.js
+++ nodejs-service/express/express-server.js
@@ -4,6 +4,17 @@ const config = require('../config');
 const app = express();
 app.use(cors());
 
+function ignoreFavicon(req, res, next) {
+    if (req.originalUrl === '/favicon.ico') {
+        res.status(204).json({ nope: true });
+    } else {
+        next();
+    }
+}
+
+app.use(ignoreFavicon);
+
+
 const port = config.app.port;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
